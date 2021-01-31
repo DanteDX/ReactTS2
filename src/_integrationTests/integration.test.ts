24635d4177c32d78adce8559acf5b7dafcc3db60
dantedxwolf <teacher.martin@gmail.com>
2021-01-31 13:41:00 +0600

    integration tests added

---
+++ src/_integrationTests/integration.test.ts
@@ -0,0 +1,20 @@
+import { AddDirector } from "../actions/DirectorAction";
+import store from "../store/store";
+
+
+describe('Testing AddDiretor Action', function () {
+  test('one', function () {
+    let expectedState = [
+      { directorName: "", directorId: 0, directorPosition: "" },
+      { directorName: "Shadman", directorId: 999, directorPosition: "Director" }
+    ];
+    const info = {
+      directorName: 'Shadman',
+      directorId: 999,
+      directorPosition: 'Director'
+    };
+    store.dispatch<any>(AddDirector(info));
+    const newState = store.getState();
+    expect(newState.directorsInformation).toStrictEqual(expectedState);
+  })
+})
